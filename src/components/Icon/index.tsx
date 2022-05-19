cf8fc8d0c440937af24c22aee8be95d6f7b60199
Jan Maliński <jan.malinski@stxnext.pl>
2022-05-19 17:58:03 +0200

    add react-native-vector-icons and Icon component

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Icon as ElementsIcon, IconProps as ElementsIconProps } from 'react-native-elements';
+
+import { palette } from 'app/config/styles';
+
+export type IconProps = ElementsIconProps;
+
+const Icon = ({ color = palette.primaryDefault, ...props }: IconProps) => (
+    //@ts-ignore
+  <ElementsIcon type="material-community" color={color} {...props} />
+);
+
+export default Icon
