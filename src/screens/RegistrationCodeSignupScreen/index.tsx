75478c80e54f5ce9d37a2025b24a3f4b9963247d
Jan Maliński <jan.malinski@stxnext.pl>
2022-06-15 15:46:00 +0200

    prevent come back to previous screen when verification email sent - RegistrationCodeSignupScreen

--- src/screens/RegistrationCodeSignupScreen/index.tsx
+++ src/screens/RegistrationCodeSignupScreen/index.tsx
@@ -1,12 +1,13 @@
-import React, { useCallback } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { verifyRequest } from 'app/store/actions';
 import { FullScreenTemplate } from 'app/components';
-import { selectors } from 'app/store';
+import { selectors, actions } from 'app/store';
 import * as Types from 'app/types';
 
 import { RegistrationCodeSignUpForm, RegistrationCodeSignUpFormData } from './components/RegistrationCodeSignupForm';
+
 
 export type Props = Types.RootStackScreenProps<Types.Route.RegistrationCodeSignUp>;
 
@@ -14,9 +15,20 @@ const initialValues: RegistrationCodeSignUpFormData = {
   code: ''
 };
 
-export const RegistrationCodeSignUpScreen = () => {
+export const RegistrationCodeSignUpScreen = ({ navigation }: Props) => {
+
   const isLoading = useSelector(selectors.isLoading);
+  const isVerificationEmailSent = useSelector(selectors.isVerificationEmailSent)
   const dispatch = useDispatch();
+
+  useEffect(()=> navigation.addListener('beforeRemove', e => {
+    if(isVerificationEmailSent === false){
+      console.log('NOT_SENT')
+      return;
+    }
+    e.preventDefault();
+    dispatch(actions.setAlert('Check your email inbox. And submit form with provided verification code', 'info'))
+  }),[navigation, isVerificationEmailSent])
 
   const registerCodeSignUpHandler = useCallback((values: RegistrationCodeSignUpFormData) => {
     dispatch(verifyRequest(values));
